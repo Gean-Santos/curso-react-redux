aa36df0c5e88c05ea486ed9cfeb2e0ab71270ae2
Gean-Santos <geansantos_15@hotmail.com>
2020-05-20 22:07:09 -0300

    Repeat with map - challenge

--- fundamentos-react/src/components/repeticao/TabelaPrecos.jsx
+++ fundamentos-react/src/components/repeticao/TabelaPrecos.jsx
@@ -4,7 +4,7 @@ import produtos from '../../data/produtos';
 import './precos.css'
 
 export default props => {
-  let val = 0
+  let val = 0;
   const produtosMap = produtos.map(produto => {
     val = produto.preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
     return (
@@ -14,8 +14,8 @@ export default props => {
         <td>{val}</td>
       </tr>
     );
-  }
-  )
+  });
+
   return (
     <div style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <table>
