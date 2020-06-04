3acb9cce898d7bb875f920c2aa023f20cae669a1
Gean-Santos <geansantos_15@hotmail.com>
2020-06-03 21:28:51 -0300

    Creating iconButton

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
 
 export default props => (
   <div role="form" className="todoForm">
-    <Grid cols="12 9 10">
+    <Grid cols='12 9 10'>
       <input id="description" className="form-control" 
         placeholder="Adicione uma tarefa"/>
     </Grid>
-    <Grid cols="12 3 2">
-      <button className="btn btn-primary">
-        <i className="fa fa-plus"></i>
-      </button>
+    <Grid cols='12 3 2'>
+      <IconButton style="primary" icon="plus" />
     </Grid>
   </div>
 );
