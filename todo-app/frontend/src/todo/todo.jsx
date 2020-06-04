6cf10eb7b09ea48eaabd71466d0ed0118e1bacfb
Gean-Santos <geansantos_15@hotmail.com>
2020-06-03 22:06:13 -0300

    Add input event

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -7,16 +7,31 @@ import TodoList from './todoList';
 export default class extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      description: '',
+      list: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
   }
+
   handleAdd() {
-    console.log(this);
+    console.log(this.state.description);
   }
+
+  handleChange(e) {
+    this.setState({...this.state, description: e.target.value});
+  }
+
   render() {
     return (
       <div>
         <PageHeader name='Tarefas' small='Cadastro' />
-        <TodoForm handleAdd={this.handleAdd} />
+        <TodoForm handleAdd={this.handleAdd} 
+          description={this.state.description} 
+          handleChange={this.handleChange}
+        />
         <TodoList />
       </div>
     );
