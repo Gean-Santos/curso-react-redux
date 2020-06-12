4e869e32baa7b20a64bb77af273838de7046b201
Gean-Santos <geansantos_15@hotmail.com>
2020-06-11 23:58:08 -0300

    Editando o componente Intervalo

--- redux-simples/src/components/Intervalo.jsx
+++ redux-simples/src/components/Intervalo.jsx
@@ -4,15 +4,17 @@ import Card from './Card';
 
 export default props => {
   return (
-    <Card title="Intervalo de Números">
-      <span>
-        <strong>Mínimo</strong>
-        <input type="number" value={0}/>
-      </span>
-      <span>
-        <strong>Mínimo</strong>
-        <input type="number" value={0}/>
-      </span>
+    <Card title="Intervalo de Números" red>
+      <div className="Intervalo">  
+        <span>
+          <strong>Mínimo:</strong>
+          <input type="number" value={0} readOnly />
+        </span>
+        <span>
+          <strong>Máximo:</strong>
+          <input type="number" value={10} readOnly />
+        </span>
+      </div>
     </Card>
   );
 }
